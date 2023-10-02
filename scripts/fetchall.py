c5dbf81fdb3d56f5ac4b3b22b5e9d8cbec3549f4
Jesse Mittertreiner <jessemittertreiner@gmail.com>
2023-10-02 13:05:33 +0200

    added new kingfisher function but it's not finished

--- scripts/fetchall.py
+++ scripts/fetchall.py
@@ -1,20 +1,26 @@
 import subprocess
 import os
 import shutil
-
+import argparse
+from scripts.pipeline import get_ids
 
 current_pwd = os.getcwd()
 output_dir = "reads"
 
-def get_prefetch():
-    id = input("Please enter the id of the run or of a file you want to generate fastq files for: ")
-    id = id.strip()
+
+def sra(id):
+    for id in get_ids().keys():
+        sra_get_prefetch(id)
+    sra_get_fastq_files()
+    
+
+def sra_get_prefetch(id):
     prefetch = f"prefetch {id}"
     print(f"Prefetching {id}!")
     subprocess.call(prefetch, shell=True)
 
 
-def get_fastq_files():
+def sra_get_fastq_files():
     for i in os.listdir('.'):
         if i.startswith("SRR"):
             sra_file = f"{current_pwd}/{i}/{i}.sra"
@@ -24,23 +30,29 @@ def get_fastq_files():
             subprocess.call(fastq_dump, shell=True)
 
 
-def move_files():
-    fastq_files = [f for f in os.listdir('.') if f.endswith('.fastq')]
-    if not os.path.exists(f"{current_pwd}/{output_dir}"):
-        os.mkdir(f"{current_pwd}/{output_dir}")
-    for fastq_file in fastq_files:
-        shutil.move(f"{current_pwd}/{fastq_file}", f"{current_pwd}/{output_dir}")
+def kingfisher():
+    for id in get_ids().keys():
+        kingfisher_fetch = f"kingfisher get -r {id} -m ena-ftp"
+        print(f"Fetching {id} with kingfisher!")
+        subprocess.call(kingfisher_fetch, shell=True)
 
-def remove_prefetch():
-    for dir in os.listdir('.'):
-        if os.path.isdir(f"{current_pwd}/{dir}") and dir != output_dir:
-            print(f"Removing {dir}!")
-            shutil.rmtree(f"{current_pwd}/{dir}")
+
+# def move_files():
+#     fastq_files = [f for f in os.listdir('.') if f.endswith('.fastq')]
+#     if not os.path.exists(f"{current_pwd}/{output_dir}"):
+#         os.mkdir(f"{current_pwd}/{output_dir}")
+#     for fastq_file in fastq_files:
+#         shutil.move(f"{current_pwd}/{fastq_file}", f"{current_pwd}/{output_dir}")
+
+# def remove_prefetch():
+#     for dir in os.listdir('.'):
+#         if os.path.isdir(f"{current_pwd}/{dir}") and dir != output_dir:
+#             print(f"Removing {dir}!")
+#             shutil.rmtree(f"{current_pwd}/{dir}")
 
 if __name__ == "__main__":
-    get_prefetch()
-    get_fastq_files()
-    move_files()
-    remove_prefetch()
+    pass
+    # move_files()
+    # remove_prefetch()
     
     
