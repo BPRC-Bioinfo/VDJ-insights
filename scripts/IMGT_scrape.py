e0a4cfbeb9407c728031fe52d89def27aaee660a
AntiCakejesCult <jessemittertreiner@gmail.com>
2024-07-03 07:36:16 +0000

    IMGT_scrape update + html

--- scripts/IMGT_scrape.py
+++ scripts/IMGT_scrape.py
@@ -1,3 +1,4 @@
+import re
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -5,19 +6,22 @@ import argparse
 from pathlib import Path
 from urllib.parse import urlencode
 from Bio import SeqIO
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 from logger import custom_logger
 
 logger = custom_logger(__name__)
+log_info = []  # List to collect log information
+fasta_files_info = []  # List to collect fasta file information
 
 
 def make_dir(dir):
     """
-    Create an directory when not existing.
+    Create a directory when not existing.
 
     Args:
         location (str): Path of the directory to create.
     """
-
     Path(dir).mkdir(parents=True, exist_ok=True)
 
 
@@ -33,6 +37,8 @@ def cleanup(directory):
     for file in directory.glob("*.fasta"):
         Path(file).unlink()
     Path(directory).rmdir()
+    log_info.append(
+        {"message": f"Deleted folder: {directory.name}, because --cleanup was selected", "level": "warning"})
     logger.info(
         f"Deleting folder: {directory.name}, because --cleanup was selected")
 
@@ -60,7 +66,6 @@ def create_library(directory: Path, simple_headers):
     append the contents of the current fasta file to the library.fasta file.
     In the end there is logged that the library is created.
 
-
     Args:
         directory (Path): Path to the directory.
     """
@@ -72,6 +77,8 @@ def create_library(directory: Path, simple_headers):
                 if simple_headers:
                     record.description = edit_header(record.description)
                 w.write(f">{record.description}\n{record.seq.upper()}\n")
+    log_info.append(
+        {"message": "Library created from generated files.", "level": "success"})
     logger.info("Creating a library from generated files.")
 
 
@@ -121,13 +128,40 @@ def scrape(response):
     for p in paragraphs:
         seq = p.text.strip()
         if ">" in seq:
-            logger.info(f"Succeeded to retrieve sequences from IMGT")
+            log_info.append(
+                {"message": "Succeeded to retrieve sequences from IMGT.", "level": "success"})
+            logger.info("Succeeded to retrieve sequences from IMGT")
             return seq
+
+
+def file_empty(file_path):
+    """
+    Checks if a file is empty by evaluating its size using the pathlib module. 
+    This function assesses whether the file at the specified path exists and, if it does, 
+    whether its size is zero bytes. It uses the `Path` object from `pathlib` to interact with the 
+    file system, making it more intuitive and readable compared to older os-based methods.
+
+    Args:
+    file_path (str or Path): The path to the file to check. Can be a string or a Path object.
+
+    Returns:
+    bool: True if the file exists and is empty (size is 0 bytes), False otherwise.
+    """
+    path = Path(file_path)
+    return path.exists() and path.stat().st_size == 0
+
+
+def set_release():
+    response = requests.get("https://www.imgt.org/vquest/refseqh.html")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    release = soup.find(
+        'h2', string=lambda text: text and 'IMGT/V-QUEST reference directory' in text)
+    return str(re.findall(r'\(.*?\)', release.text.strip())[0][1:-1])
 
 
 def write_sequence(name, directory, sequence):
     """
-    Write the found VDJ segment sequenecs. It first establish a base fasta file. 
+    Write the found VDJ segment sequences. It first establishes a base fasta file. 
     Then it writes the VDJ sequences to the fasta file and logs that 
     sequence are written to the file.
 
@@ -138,42 +172,60 @@ def write_sequence(name, directory, sequence):
         for a current VDJ segment.
     """
     file = directory / f"{name}.fasta"
+    log_info.append(
+        {"message": f"Writing sequences from {name} to {file.name}.", "level": "success"})
     logger.info(f"Writing sequences from {name} to {file.name}")
     with open(file, 'w') as f:
         f.write(str(sequence) + "\n")
+    # Count the number of entries in the fasta file
+    count = sum(1 for _ in SeqIO.parse(file, "fasta"))
+    fasta_files_info.append({"name": file.name, "entries": count})
+    return file_empty(file)
+
+    # Count the number of entries in the fasta file
+    count = sum(1 for _ in SeqIO.parse(file, "fasta"))
+    fasta_files_info.append({"name": file.name, "entries": count})
 
 
-def fetch_sequence(segment, directory, species, frame):
+def fetch_sequence(segment, directory, species, frame, retry_limit=3):
     """
-    Uses the constrcted url and fetches the response based on the url. 
-    It checks the response code. If it is equal 200 the response is
-    scraped otherwise it is logged that it failed to retrieve data from IMGT server.
-    it also checks if something is found after scraping. 
-    Otherwise it logs that it could not scrape any data from the response.
-    In the end a 2 second sleep it called to avoid overloading the IMGT server.
-
+    Fetches sequence data from IMGT server using a constructed URL and handles failures with specified retry logic.
+    Implements differentiated wait times based on the cause of retry need.
 
     Args:
-        segment (str): The segment type itself.
-        directory (Path): Path to the directory.
-        species (str): The chosen species, which is choses from the
-        argparse list for species.
-        frame (str): the chosen frame, which is choses from the argparse
-        list for regarding frame.
+        segment (str): The segment type.
+        directory (Path): Directory path where sequence data is to be stored.
+        species (str): Species name.
+        frame (str): Frame specification.
+        retry_limit (int): Maximum number of retries for fetching data.
     """
-    url = construct_url(segment, species, frame)
-    response = requests.get(url)
-    if response.status_code == 200:
-        sequence = scrape(response)
-        if sequence:
-            write_sequence(segment, directory, sequence)
+    for attempt in range(retry_limit):
+        url = construct_url(segment, species, frame)
+        response = requests.get(url)
+        sleep_time = 2
+        if response.status_code == 200:
+            sequence = scrape(response)
+            if sequence:
+                empty_value = write_sequence(segment, directory, sequence)
+                if not empty_value:
+                    logger.info("Sequence successfully retrieved and written.")
+                    break
+                else:
+                    sleep_time = 30
+                    logger.warning(
+                        "Retrieved sequence was empty. Will retry after extended wait.")
+            else:
+                logger.warning(
+                    f"No sequences found for {segment} of {species}.")
         else:
-            logger.warning(f"No sequences found for {segment} of {species}.")
-    else:
-        logger.warning(
-            f"Failed to fetch data for {segment} of {species}. Status code: {response.status_code}")
-    logger.info("Waiting 2 seconds to avoid overloading the IMGT server!")
-    time.sleep(2)
+            logger.warning(
+                f"Failed to fetch data for {segment} of {species} with status code: {response.status_code}")
+        logger.info(
+            f"Waiting {sleep_time} seconds to avoid overloading the IMGT server.")
+        time.sleep(sleep_time)
+    if attempt == retry_limit - 1 and response.status_code == 200 and not sequence:
+        logger.error(
+            f"Max retries reached for {segment} of {species} without successful data retrieval.")
 
 
 def scrape_IMGT(species, immune_type, directory, frame):
@@ -188,7 +240,6 @@ def scrape_IMGT(species, immune_type, directory, frame):
     Otherwise the segment sequences are fetched and logged.
 
     Args:
-
         species (str): The chosen species, which is choses from the
         argparse list for species.
         immune_type (str): The type of receptor that is being used. 
@@ -208,14 +259,25 @@ def scrape_IMGT(species, immune_type, directory, frame):
         ]
     }
     if not directory.exists():
+        log_info.append(
+            {"message": f"Folder created: {directory.name}.", "level": "success"})
         logger.info(f"Folder {directory.name} does not exist, creating it!")
         make_dir(directory)
     for segment in segments[immune_type]:
+        segment_file = directory / f"{segment}.fasta"
+        log_info.append(
+            {"message": f"Retrieving sequences from IMGT for the {segment} of {species}.", "level": "info"})
         logger.info(
             f"Retrieving sequences from IMGT for the {segment} of {species}")
-        if not Path(directory / f"{segment}.fasta").exists():
+        if not Path(segment_file).exists():
             fetch_sequence(segment, directory, species, frame)
         else:
+            # Count the number of entries in the fasta file
+            count = sum(1 for _ in SeqIO.parse(segment_file, "fasta"))
+            fasta_files_info.append(
+                {"name": segment_file.name, "entries": count})
+            log_info.append(
+                {"message": f"File exists: {segment}.fasta already exists, skipping!", "level": "info"})
             logger.info(f"File {segment}.fasta already exists skipping!")
             time.sleep(2)
 
@@ -319,6 +381,38 @@ def argparser_setup():
     return args
 
 
+def save_log_to_html(release, fasta_files_info, file_path, args):
+    """
+    Save the collected log information to an HTML file using Jinja2 template.
+
+    Args:
+        log_info (list): List of log strings to save.
+        fasta_files_info (list): List of dictionaries containing fasta file names and number of entries.
+        file_path (str): Path to the HTML file.
+        args (argparse.Namespace): Parsed command line arguments.
+    """
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('source/template/template.html')
+
+    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    html_content = template.render(
+        date_time=date_time,
+        set_release=release,
+        species=args.species,
+        type=args.type,
+        output=args.output,
+        frame_selection=args.frame_selection,
+        create_library=args.create_library,
+        cleanup=args.cleanup,
+        simple_headers=args.simple_headers,
+        fasta_files=fasta_files_info
+    )
+
+    with open(file_path, 'w') as html_file:
+        html_file.write(html_content)
+
+
 def main():
     """
     Main function of the IMGT_scrape script. 
@@ -331,7 +425,10 @@ def main():
     created and/or that the individual fasta files of the segments
     need to be removed. Lastly there is logged that the scrape is finished.
     """
+    global args  # Make args global to access it in save_log_to_html function
     args = argparser_setup()
+    log_info.append(
+        {"message": f"Starting scrape for species: {args.species}, type: {args.type}.", "level": "info"})
     logger.info(
         f"Starting scrape for species: {args.species}, type: {args.type}")
 
@@ -346,7 +443,13 @@ def main():
     if args.cleanup:
         cleanup(output_dir)
 
+    log_info.append(
+        {"message": "Scrape completed successfully.", "level": "success"})
     logger.info("Scrape completed successfully.")
+
+    # Save log to HTML
+    save_log_to_html(set_release(), fasta_files_info,
+                     "source/html/scrape_report.html", args)
 
 
 if __name__ == '__main__':
