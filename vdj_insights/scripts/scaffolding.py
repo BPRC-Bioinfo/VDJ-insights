36bb3a602d120cb82810e117c935e15d3eee8f3d
sayedjm <jaimymohammadi@gmail.com>
2025-05-30 08:28:29 +0000

    Add json to known witch contigs on a scaffold

--- vdj_insights/scripts/scaffolding.py
+++ vdj_insights/scripts/scaffolding.py
@@ -3,15 +3,42 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 import subprocess
 import shutil
+import json
+import pandas as pd
 
 from .util import make_dir, calculate_available_resources
+from .logger import console_logger, file_logger
+
+console_log = console_logger(__name__)
+file_log = file_logger(__name__)
+
+
+def make_json_scaffolds():
+    cwd = Path.cwd()
+    ragtag_path = cwd / Path("tmp/ragtag")
+
+    scaffold_agp_files = [Path(agp_file) for agp_file in ragtag_path.rglob('*') if agp_file.suffix.lower() == '.agp']
+    all_scaffolds = {}
+    for agp_file in scaffold_agp_files:
+        try:
+            assembly = agp_file.parent.name
+            agp_df = pd.read_csv(agp_file, sep="\t", header=None, skiprows=2)
+            agp_df = agp_df[agp_df[4] == "W"]
+            scaffold_contigs = agp_df.groupby(0)[5].apply(list)
+            scaffold_dict = scaffold_contigs.to_dict()
+            all_scaffolds[assembly] = scaffold_dict
+        except Exception as e:
+            file_log.error(f"Error processing {agp_file}: {e}")
+
+    json_path = ragtag_path / "ragtag_scaffolds.json"
+    with open(json_path, 'w') as f:
+        json.dump(all_scaffolds, f, indent=2)
 
 
 def run_scaffolding(assembly_file: str, reference: str, scaffolding_dir: str):
     cwd = Path.cwd()
 
     assembly_name = assembly_file.stem
-    #if not assembly_name in reference_name:
     ragtag_output = cwd / Path("tmp/ragtag") / assembly_name
     make_dir(ragtag_output)
 
@@ -28,7 +55,6 @@ def run_scaffolding(assembly_file: str, reference: str, scaffolding_dir: str):
         shutil.move(str(original_scaffold), str(renamed_scaffold))
 
 
-
 def scaffolding_main(reference: str, assembly_dir: Path, scaffolding_dir: str,  threads: int) -> Path:
     cwd = Path.cwd()
     assembly_files = [file for ext in ["*.fna", "*.fasta", "*.fa"] for file in (cwd / assembly_dir).glob(ext)]
@@ -41,5 +67,7 @@ def scaffolding_main(reference: str, assembly_dir: Path, scaffolding_dir: str,  
             with tqdm(total=total_tasks, desc='Scaffolding:', unit="Assemblies") as pbar:
                 for future in as_completed(futures):
                     pbar.update(1)
+
+    make_json_scaffolds()
     return Path(scaffolding_dir)
 
